286250cd2888a4cf77f6123ce1b81650a0825874
Wolfgang Roth <roth@tugraz.at>
2018-06-28 19:03:11 +0200

    Added classification experiment with diagonal matrices

--- models/GMMClassifierDiag.py
+++ models/GMMClassifierDiag.py
@@ -1,5 +1,5 @@
 '''
-Theano computation graph that defines the hybrid GMM objectives for DPLR
+Theano computation graph that defines the hybrid GMM objectives for diagonal
 covariance matrices from [1].
 
 [1] Roth W., Peharz R., Tschiatschek S., Pernkopf F., Hybrid generative-
@@ -13,19 +13,14 @@ import numpy as np
 import theano
 import theano.tensor as T
 
-from theanoOpLogDetPSD import logdet_psd
-
-class GMMClassifier:
-    def __init__(self, C, D, K, S, rng_state=None, epsilon=1e-2, use_precision=True, tradeoff=0.5, gamma=1, eta=10, init_params=None):
+class GMMClassifierDiag:
+    def __init__(self, C, D, K, rng_state=None, epsilon=1e-2, use_precision=True, tradeoff=0.5, gamma=1, eta=10, init_params=None):
         '''
         Constructs the Theano computation graph for the given parameters
         
         C: Number of classes
         D: Number of input features
         K: List of length C containing the number of components per class
-        S: Number of dimensions of the low-rank approximation of the DPLR matrix
-          structure. Note that this parameter is actually called 'R' in the
-          paper.
         rng_state: Random number generator seed to use if the parameters should
           be initialized randomly. This parameter is ignored if 'init_params' is
           given.
@@ -40,14 +35,13 @@ class GMMClassifier:
           obtained with the EM algorithm are provided here. init_params must be
           a five-tuple containing
             - mu_vals (K_all x D): Mean values for each component
-            - s_vals (K_all x D x S): Low-rank matrices for each component
             - d_rho_vals (D x K_all): Diagonal variances for each component (inverse softplus values)
             - prior_k_rho_vals (K_all): Logits of the component priors
             - prior_c_rho_vals (C): Logits of the class priors
           with K_all = sum(K). The component parameters are stored linearly for
           all classes. E.g. the first K[0] entries correspond to components of
           class 0. If precisions are used instead of covariances, the use of
-          s_vals and d_rho_vals changes accordingly.
+          d_rho_vals changes accordingly.
         '''
         self.x = T.matrix('x')
         self.t = T.ivector('t')
@@ -61,78 +55,48 @@ class GMMClassifier:
         if init_params is None:
             rng = np.random.RandomState(rng_state)
             mu_vals = rng.normal(0., 1., size=(K_all, D))
-            s_vals = rng.normal(0., 1., size=(K_all, D, S))
             d_rho_vals = rng.normal(0, 0.1, size=(D, K_all))
             prior_k_rho_vals = np.zeros((K_all,))
             prior_c_rho_vals = np.zeros((C,))
         else:
-            mu_vals, s_vals, d_rho_vals, prior_k_rho_vals, prior_c_rho_vals = init_params
+            mu_vals, d_rho_vals, prior_k_rho_vals, prior_c_rho_vals = init_params
             assert mu_vals.shape == (K_all, D)
-            assert s_vals.shape == (K_all, D, S)
             assert d_rho_vals.shape == (D, K_all)
             assert prior_k_rho_vals.shape == (K_all,)
             assert prior_c_rho_vals.shape == (C,)
 
         mu_vals = np.asarray(mu_vals, dtype=theano.config.floatX)
-        s_vals = np.asarray(s_vals, dtype=theano.config.floatX)
         d_rho_vals = np.asarray(d_rho_vals, dtype=theano.config.floatX)
         prior_k_rho_vals = np.asarray(prior_k_rho_vals, dtype=theano.config.floatX)
         prior_c_rho_vals = np.asarray(prior_c_rho_vals, dtype=theano.config.floatX)
         
         # Shared variables
         self.means = theano.shared(mu_vals, name='means', borrow=True) 
-        self.s = theano.shared(s_vals, name='s', borrow=True)
         self.d_rho = theano.shared(d_rho_vals, name='d_rho', borrow=True)
         self.prior_k_rho = theano.shared(prior_k_rho_vals, name='prior_k_rho', borrow=True)
         self.prior_c_rho = theano.shared(prior_c_rho_vals, name='prior_c_rho', borrow=True)
-        self.params = [self.means, self.s, self.d_rho, self.prior_k_rho, self.prior_c_rho]
+        self.params = [self.means, self.d_rho, self.prior_k_rho, self.prior_c_rho]
 
         self.d = T.nnet.softplus(self.d_rho) + self.epsilon
         if use_precision == True:
-            # s and d are used to represent precision matrices
-            self.exponent = T.dot(self.x ** 2, self.d) #xDx
-            self.exponent -= 2 * T.dot(self.x, self.d * self.means.T) #-2xDm
-            self.exponent += T.sum(self.means ** 2 * self.d.T, axis=1) # mDm
-            self.exponent += T.sum(T.dot(self.x, self.s) ** 2, axis=2) # xSSx
-            self.exponent -= 2 * T.sum(T.dot(self.x, self.s) * T.sum(self.s * self.means[:,:,None], axis=1)[None,:,:], axis=2) # -2xSSm
-            self.exponent += T.sum(T.sum(self.s * self.means[:,:,None], axis=1) ** 2, axis=1) # mSSm
-            self.exponent *= -0.5
-    
-            eye_S = T.eye(S, dtype=theano.config.floatX)
-            self.aux_matrix = T.batched_tensordot(self.s / self.d.T[:,:,None], self.s, axes=(1, 1)) + eye_S
-            self.aux_logdet, _ = theano.scan(fn=lambda aux: logdet_psd(aux),
-                                         outputs_info=None,
-                                         sequences=self.aux_matrix,
-                                         non_sequences=None)
-            self.logdet = T.sum(T.log(self.d), axis=0) + self.aux_logdet
-
+            self.val1 = T.dot(self.x ** 2, self.d) # NxK
+            self.val2 = T.sum(self.means ** 2 * self.d.T, axis=1) # K
+            self.val3 = -2. * T.dot(self.x, self.d * self.means.T) # NxK
+            self.exponent = -0.5 * (self.val1 + self.val2 + self.val3) #NxK
+            
+            self.logdet = T.sum(T.log(self.d), axis=0) # K
+            
             # logpK contains all log probabilities of all components in an (N x sum(K)) array
             # Note that the log component priors are not added yet
             self.logpK = -0.5 * D * T.log(2. * np.pi) + 0.5 * self.logdet  + self.exponent
         else:
-            # s and d are used to represent covariance matrices
-            eye_S = T.eye(S, dtype=theano.config.floatX)
-            self.aux_matrix = T.batched_tensordot(self.s / self.d.T[:,:,None], self.s, axes=(1, 1)) + eye_S
-            (self.aux_inv, self.aux_logdet), _ = theano.scan(fn=lambda aux: [T.nlinalg.matrix_inverse(aux), logdet_psd(aux)],
-                                                         outputs_info=None,
-                                                         sequences=[self.aux_matrix],
-                                                         non_sequences=None)
-            self.logdet = T.sum(T.log(self.d), axis=0) + self.aux_logdet
-
-            # Product inv(d) * s for all K --> K x D x S
-            self.rs = self.s / self.d.T[:,:,None]
-            # Product inv(d) * s * aux_inv for all K --> K x D x S
-            self.ls = T.batched_dot(self.rs, self.aux_inv)
-
-            # s and d are used to represent covariance matrices
-            self.exponent = T.dot(self.x ** 2, 1. / self.d) #xDx
-            self.exponent -= 2 * T.dot(self.x, (1. / self.d) * self.means.T) #-2xDm
-            self.exponent += T.sum(self.means ** 2 * (1. / self.d.T), axis=1) # mDm
-            self.exponent -= T.sum(T.dot(self.x, self.ls) * T.dot(self.x, self.rs), axis=2) # -x ls rs x
-            self.exponent += 2 * T.sum(T.dot(self.x, self.ls) * T.sum(self.rs * self.means[:,:,None], axis=1)[None,:,:], axis=2) # 2x ls rs m
-            self.exponent -= T.sum(T.sum(self.ls * self.means[:,:,None], axis=1) * T.sum(self.rs * self.means[:,:,None], axis=1), axis=1) # -m ls rs m
-            self.exponent *= -0.5
-
+            self.val1 = T.dot(self.x ** 2, 1 / self.d) # NxK
+            self.val2 = T.sum(self.means ** 2 / self.d.T, axis=1) # K
+            self.val3 = -2. * T.dot(self.x, (1 / self.d) * self.means.T) # NxK
+            self.exponent = -0.5 * (self.val1 + self.val2 + self.val3) #NxK
+            
+            self.logdet = T.sum(T.log(self.d), axis=0) # K
+            
             # logpK contains all log probabilities of all components in an (N x sum(K)) array
             # Note that the log component priors are not added yet
             self.logpK = -0.5 * D * T.log(2. * np.pi) - 0.5 * self.logdet + self.exponent
@@ -183,7 +147,6 @@ class GMMClassifier:
 
     def getParameters(self):
         params = {'mu' : self.means.get_value(borrow=True),
-                  's' : self.s.get_value(borrow=True),
                   'd_rho' : self.d_rho.get_value(borrow=True),
                   'prior_k_rho' : self.prior_k_rho.get_value(borrow=True),
                   'prior_c_rho' : self.prior_c_rho.get_value(borrow=True)}
--- models/modelUtils.py
+++ models/modelUtils.py
@@ -36,11 +36,39 @@ def classifyGmmFullCov(gmm_params, K, x, t):
     ce = np.mean(t != np.argmax(logl, axis=1))
     return ce
 
+def classifyGmmDiagCov(gmm_params, K, x, t):
+    '''
+    Returns the classification error for GMMs with diagonal covariance matrices.
+    '''
+    C = len(K)
+    logl = np.zeros((x.shape[0], C))
+     
+    for c in range(C):
+        logl_class = np.zeros((x.shape[0], K[c]))
+         
+        alpha = gmm_params['alpha_%d' % (c)]
+        mu = gmm_params['mu_%d' % (c)]
+        Lambda = gmm_params['Lambda_%d' % (c)]
+         
+        for k in range(K[c]):
+            x_sub_mu = x - mu[k, :]
+            logdet = np.sum(np.log(Lambda[k,:]))
+            logl_class[:, k] = -x_sub_mu.shape[1] * 0.5 * np.log(2 * np.pi) + 0.5 * logdet - 0.5 * np.sum((x_sub_mu ** 2.) * Lambda[k,:], axis=1)
+             
+        logl_class = logl_class + np.log(alpha)
+        logl[:, c] = logsumexp(logl_class, axis=1)
+    logl += np.log(gmm_params['prior'])
+ 
+    ce = np.mean(t != np.argmax(logl, axis=1))
+    return ce
+
 def getDplrParameters(gmm_params, S, epsilon, use_precision=False):
     '''
-    Converts full covariance matrices into the DPLR matrix structure described
-    in [1]. The model parameters returned by this function can be readily used
-    as initial parameters for GMMClassifier
+    Converts GMM parameters with full covariance matrices stored in a dictionary
+    (as obtained from trainGmmEM) into the format that is used by the
+    GMMClassifier class. The full covariance matrices are converted into the
+    DPLR matrix structure described in [1]. The model parameters returned by
+    this function can be readily used as initial parameters for GMMClassifier.
     
     [1] Roth W., Peharz R., Tschiatschek S., Pernkopf F., Hybrid generative-
     discriminative training of Gaussian mixture models, Pattern Recognition
@@ -73,7 +101,7 @@ def getDplrParameters(gmm_params, S, epsilon, use_precision=False):
                 init_mu[k_idx, :] = gmm_params['mu_%d' % (c)][k, :]
                 init_s[k_idx, :, :] = s
                 max_d_0 = np.maximum(d, 0) # log-sum-exp trick for numerical stability
-                init_d_rho[:, k_idx] = np.log(np.exp(d - max_d_0) - np.exp(0 - max_d_0)) + max_d_0
+                init_d_rho[:, k_idx] = np.log(np.exp(d - max_d_0) - np.exp(0 - max_d_0)) + max_d_0 # inverse softplus
         else:
             for k in range(K[c]):
                 k_idx = k1 + k
@@ -81,10 +109,46 @@ def getDplrParameters(gmm_params, S, epsilon, use_precision=False):
                 s = v * np.sqrt(w)
                 Sigma = np.dot(s, s.T)
                 d = np.diag(gmm_params['Sigma_%d' % (c)][k, :, :] - Sigma)
-                d = np.maximum(d - epsilon, 1e-8)
+                d = np.maximum(d - epsilon, 1e-8) # epsilon is added again in GMMClassifier
                 init_mu[k_idx, :] = gmm_params['mu_%d' % (c)][k, :]
                 init_s[k_idx, :, :] = s
                 max_d_0 = np.maximum(d, 0) # log-sum-exp trick for numerical stability
-                init_d_rho[:, k_idx] = np.log(np.exp(d - max_d_0) - np.exp(0 - max_d_0)) + max_d_0
+                init_d_rho[:, k_idx] = np.log(np.exp(d - max_d_0) - np.exp(0 - max_d_0)) + max_d_0 # inverse softplus
 
     return init_mu, init_s, init_d_rho, init_prior_k_rho, init_prior_c_rho
+
+def getDiagParameters(gmm_params, epsilon, use_precision=False):
+    '''
+    Converts GMM parameters with diagonal covariance matrices stored in a
+    dictionary (as obtained from trainGmmEM) into the format that is used by the
+    GMMClassifierDiag class. The model parameters returned by this function can
+    be readily used as initial parameters for GMMClassifierDiag.
+    '''
+    C = len(gmm_params['prior'])
+    D = gmm_params['mu_0'].shape[1]
+    K = []
+    for c in range(C):
+        K.append(len(gmm_params['alpha_%d' % (c)]))
+    K_all = np.sum(K)
+    init_mu = np.zeros((K_all, D))
+    init_d_rho = np.zeros((D, K_all))
+    init_prior_k_rho = np.zeros((K_all,))
+    init_prior_c_rho = np.log(gmm_params['prior'])
+    
+    for c in range(C):
+        k1 = int(np.sum(K[:c]))
+        k2 = k1 + int(K[c])
+        init_prior_k_rho[k1:k2] = np.log(gmm_params['alpha_%d' % (c)])
+        if use_precision:
+            for k in range(K[c]):
+                k_idx = k1 + k
+                init_mu[k_idx, :] = gmm_params['mu_%d' % (c)][k, :]
+                init_d_rho[:, k_idx] = np.log(1 - np.exp(-gmm_params['Lambda_%d' % (c)][k, :])) + gmm_params['Lambda_%d' % (c)][k, :] # inverse softplus
+        else:
+            for k in range(K[c]):
+                k_idx = k1 + k
+                init_mu[k_idx, :] = gmm_params['mu_%d' % (c)][k, :]
+                d = np.maximum(gmm_params['Sigma_%d' % (c)][k, :] - float(epsilon), 1e-15)  # epsilon is added again in GMMClassifierDiag
+                init_d_rho[:, k_idx] = np.log(1 - np.exp(-d)) + d # inverse softplus
+    
+    return init_mu, init_d_rho, init_prior_k_rho, init_prior_c_rho
